c9e1cd10cc49a3fbc89d222544f052118a411e7d
FrancescaLong <francesca_long@hotmail.com>
2015-12-10 16:26:36 -0800

    first attempt at videos helper

--- app/helpers/videos_helper.rb
+++ app/helpers/videos_helper.rb
@@ -1,2 +1,20 @@
-module ParentsHelper
+module VideosHelper
+
+	def showVideo child
+		child = Child.where("child_id = ? AND user_id = ?", child.id, user.id)
+		# if include
+		# not sure about what to include here
+	end
+
+	def hideVideo child
+		child = Child.where("child_id = ? AND user_id = ?", child.id, user.id)
+		# if hide
+		# not sure about what to include here
+	end
+
+	def shareVideo user
+		# need something here to allow users to share their videos with other users
+	end
+
+
 end
