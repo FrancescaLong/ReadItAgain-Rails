289b1a4ab60da1488b66665836dbe7b47599c157
FrancescaLong <francesca_long@hotmail.com>
2015-12-16 15:03:21 -0800

    new child form working

--- app/controllers/children_controller.rb
+++ app/controllers/children_controller.rb
@@ -20,4 +20,51 @@ class ChildrenController < ApplicationController
 		render :show
 		end
 
+
+
+
+
+
+
+	def new
+		@user = current_user
+		@logged_in = logged_in?
+		@child = Child.new
+		render :new # render new child
+	end
+
+	def create
+		child_params = params.require(:child).permit(:name)
+		@child = Child.create(child_params)
+		# @video.user.children << @child
+		current_user.children << @child
+		current_user.save
+		redirect_to "/users/#{current_user.id}"
+	end
+
+	def edit
+		id = params[:id]
+			@child = Child.find(id)
+	end
+
+	def update
+		id = params[:id]
+			child = Child.find(id)
+
+			updated_attributes = params.require(:child).permit(:name)
+			child.update_attributes(updated_attributes)
+			redirect_to child
+	end
+
+	def destroy
+		id = params[:id]
+		child = Child.find(id)
+		video = Video.find(child.video_id)
+		child.destroy
+		redirect_to "/users/#{current_user.id}"
+	end
+
+
+
+
 end
